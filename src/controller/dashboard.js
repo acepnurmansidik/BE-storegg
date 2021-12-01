129fa92471bcd07c5d73f6b89c2ba00568852b93
acep nurman sidik <acepnurmansidik@gmail.com>
2021-12-02 01:46:06 +0700

    succesfuly dashboard page

--- src/controller/dashboard.js
+++ src/controller/dashboard.js
@@ -1,9 +1,25 @@
+const Transaction = require("../models/transaction");
+const Voucher = require("../models/voucher");
+const Category = require("../models/category");
+const Player = require("../models/category");
+
 module.exports = {
   index_dashboard: async (req, res) => {
     try {
-      res.render("index",{
+      const transaction = await Transaction.countDocuments();
+      const voucher = await Voucher.countDocuments();
+      const category = await Category.countDocuments();
+      const player = await Player.countDocuments();
+
+      res.render("admin/dashboard/view_dashboard", {
         title: "Dashboard page",
-        name: req.session.user.name
+        name: req.session.user.name,
+        count: {
+          transaction,
+          category,
+          voucher,
+          player,
+        },
       });
     } catch (err) {
       console.log(err.message);
