7ee436b7a823a35ab69711b0f8327c5bfb667453
acep nurman sidik <acepnurmansidik@gmail.com>
2021-12-06 05:19:03 +0700

    successfult create history detail

--- app/player/router.js
+++ app/player/router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { landingPage, categoryPage, detailPage, historyPage, checkoutPage } = require("./controller");
+const { landingPage, categoryPage, detailPage, historyPage, checkoutPage, historyDetailPage } = require("./controller");
 const router = express.Router();
 const {playerAuthorization}=require("../../src/middleware/auth")
 
@@ -8,5 +8,6 @@ router.get("/:id/detail", detailPage);
 router.get("/category", categoryPage);
 router.post("/checkout",playerAuthorization, checkoutPage);
 router.get("/history", playerAuthorization, historyPage);
+router.get("/history/:id/detail", playerAuthorization, historyDetailPage);
 
 module.exports = router;
