6aa12ba56a0f7ef3ab5c55417fb6ec43c48efdee
acep nurman sidik <acepnurmansidik@gmail.com>
2021-12-06 22:22:39 +0700

    edit url imaga & view_player

--- app/users/controller.js
+++ app/users/controller.js
@@ -68,42 +68,42 @@ module.exports = {
     res.redirect("/");
   },
   // PLAYER =======================================
-  // indexPlayer: async (req, res) => {
-  //   try {
-  //     const alertMessage = req.flash("alertMessage");
-  //     const alertStatus = req.flash("alertStatus");
-  //     const alert = { message: alertMessage, status: alertStatus };
+  indexPlayer: async (req, res) => {
+    try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+      const alert = { message: alertMessage, status: alertStatus };
 
-  //     const players = await Player.find();
+      const players = await Player.find();
 
-  //     res.render("admin/player/view_player", {
-  //       title: "Player page",
-  //       name: req.session.user.name,
-  //       alert,
-  //       players,
-  //     });
-  //   } catch (err) {
-  //     req.flash("alertMessage", `${err.message}`);
-  //     req.flash("alertStatus", `danger`);
-  //     res.redirect("/");
-  //   }
-  // },
-  // viewDetailPlayer: async (req, res) => {
-  //   try {
-  //     const { id } = req.params;
+      res.render("admin/player/view_player", {
+        title: "Player page",
+        name: req.session.user.name,
+        alert,
+        players,
+      });
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", `danger`);
+      res.redirect("/");
+    }
+  },
+  viewDetailPlayer: async (req, res) => {
+    try {
+      const { id } = req.params;
 
-  //     const player = await Player.findOne({ _id: id });
-  //     console.log(player);
+      const player = await Player.findOne({ _id: id });
+      console.log(player);
 
-  //     res.render("admin/player/detail", {
-  //       title: "Detail player page",
-  //       name: req.session.user.name,
-  //       player,
-  //     });
-  //   } catch (err) {
-  //     req.flash("alertMessage", `${err.message}`);
-  //     req.flash("alertStatus", `danger`);
-  //     res.redirect("/");
-  //   }
-  // },
+      res.render("admin/player/detail", {
+        title: "Detail player page",
+        name: req.session.user.name,
+        player,
+      });
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", `danger`);
+      res.redirect("/");
+    }
+  },
 };
--- app/users/router.js
+++ app/users/router.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { viewSignIn, actionSignin, actionSignout } = require("./controller");
+const { viewSignIn, actionSignin, actionSignout, indexPlayer, viewDetailPlayer  } = require("./controller");
 
 router.get("/", viewSignIn);
 router.post("/", actionSignin);
 router.get("/signout", actionSignout);
 
-// const { isLogin } = require("../../src/middleware/auth");, indexPlayer, viewDetailPlayer 
-// router.use(isLogin);
+const { isLogin } = require("../../src/middleware/auth");
+router.use(isLogin);
 
-// router.get("/player", indexPlayer);
-// router.get("/player/detail/:id", viewDetailPlayer)
+router.get("/player", indexPlayer);
+router.get("/player/detail/:id", viewDetailPlayer)
 
 module.exports = router;
